dd826b4dbb491eef8d10112e3ee8750e6fd5a159
Kevin Rohan Vaz <excalibur.krv@gmail.com>
2022-10-16 17:20:15 +0530

    added evaluation

--- parsing.c
+++ parsing.c
@@ -13,6 +13,37 @@ number        :  -?[0-9]+(\\.[0-9]+)?
 
 */
 
+// use operator string to see which operation to perform
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) return x + y;
+  if (strcmp(op, "-") == 0) return x - y;
+  if (strcmp(op, "*") == 0) return x * y;
+  if (strcmp(op, "/") == 0) return x / y;
+  if (strcmp(op, "%") == 0) return x % y;
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  // if tagged as number return it directly
+  if (strstr(t -> tag, "number")) {
+    return atoi(t -> contents);
+  }
+
+  // the operator is always the second child
+  char* op = t -> children[1] -> contents;
+
+  // we store the third variable in x
+  long x = eval(t -> children[2]);
+
+  // Iterate remaining children and combining 
+  int i = 3;
+  while (strstr(t -> children[i] -> tag, "expression")) {
+    x = eval_op(x, op, eval(t -> children[i]));
+    i++;
+  } 
+
+  return x;
+}
 
 int main(int argc, char** argv) {
   // Create some parsers
@@ -24,7 +55,7 @@ int main(int argc, char** argv) {
   mpca_lang(MPCA_LANG_DEFAULT, 
   "                                                        \
     number     : /-?[0-9]+(\\.[0-9]+)?/ ;                              \
-    operator   : '+' | '-' | '*' | '/' ;                   \
+    operator   : '+' | '-' | '*' | '/' | '%' ;                   \
     expression : <number> | '(' <operator> <expression>+ ')' ;   \
     program    : /^/ <operator> <expression>+ /$/ ;              \
   ", Number, Operator, Expression, Program);
@@ -40,7 +71,9 @@ int main(int argc, char** argv) {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Program, &r)) {
       // On success print the ast
+      long result = eval(r.output);
       mpc_ast_print(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       // Otherwise print error
